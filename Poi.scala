8ae95547e2b760a93217e0fab97eabd2c79d6b9d
Iwo <iwo.naglik98@gmail.com>
2021-01-07 19:47:16 +0100

    POI etl

--- Poi.scala
+++ Poi.scala
@@ -5,12 +5,12 @@ import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions.{col, monotonically_increasing_id, row_number}
 
 
-object Day {
+object Poi {
 
   def main(args: Array[String]) {
 
     val spark = SparkSession.builder()
-      .appName("LocalizationETL")
+      .appName("PoiETL")
       //.master("local")
       .enableHiveSupport()
       .getOrCreate()
@@ -39,25 +39,43 @@ object Day {
       option("inferSchema", true).
       csv(s"/user/$username/labs/spark/externaldata/mainDataEastern.csv");
 
-    val dayDF = mainMountain
+    val PoiDF = mainMountain
       .union(mainPacific)
       .union(mainCentral)
       .union(mainEastern)
-      .select("Sunrise_Sunset", "Civil_Twilight", "Nautical_Twilight", "Astronomical_Twilight")
+      .select("Amenity",
+        "Bump",
+        "Crossing",
+        "Give_Way",
+        "Junction",
+        "No_Exit",
+        "Railway",
+        "Roundabout",
+        "Station",
+        "Stop",
+        "Traffic_Calming",
+        "Traffic_Signal",
+        "Turning_Loop")
       .distinct().na.drop("all");
 
 
-    val allDayDF = dayDF.
-      withColumn("Day_id", monotonically_increasing_id).
-      select("Day_id", "Sunrise_Sunset", "Civil_Twilight", "Nautical_Twilight", "Astronomical_Twilight")
+    val intPoiDF = PoiDF.select(
+      (col("Amenity").cast("integer") + col("Railway").cast("integer") + col("Station").cast("integer")).as("Object_poi_count"),
+      (col("Bump").cast("integer") + col("Traffic_Calming").cast("integer") + col("Traffic_Signal").cast("integer")).as("Calming_poi_count"),
+      (col("Crossing").cast("integer") + col("Junction").cast("integer") + col("Roundabout").cast("integer") + col("Turning_Loop").cast("integer")).as("Road_poi_count"),
+      (col("Give_Way").cast("integer") + col("No_Exit").cast("integer") + col("Stop").cast("integer")).as("Sign_poi_count")
+    );
+    val allPoiDF = intPoiDF.
+      withColumn("Poi_id", monotonically_increasing_id).
+      select("Poi_id", "Object_poi_count", "Calming_poi_count", "Road_poi_count", "Sign_poi_count");
 
-    val window = Window.orderBy($"Day_id")
 
-    val finalDataDF = allDayDF.withColumn("Day_id", row_number.over(window))
+    val window = Window.orderBy($"Poi_id")
 
+    val finalDataDF = allPoiDF.withColumn("Poi_id", row_number.over(window))
     //finalDataDF.show()
-    finalDataDF.write.insertInto("Day")
+    finalDataDF.write.insertInto("Poi")
 
-    println("Załadowano dane do tabeli wymiaru 'Day'")
+    println("Załadowano dane do tabeli wymiaru 'Poi'")
   }
 }
